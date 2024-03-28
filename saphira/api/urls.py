db39a8f5bb13ac96449068cf3efd98ec57b66126
FranciscoGJR <franciscoogjr@gmail.com>
2024-03-28 05:34:20 -0300

    feature: implementando etapa4

--- saphira/api/urls.py
+++ saphira/api/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.get_datetime, name="get_datetime"),
+    path("texto/<texto>", views.get_texto, name="get_texto")
 ]
--- saphira/api/views.py
+++ saphira/api/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse, JsonResponse
 
 def get_datetime(request):
     return JsonResponse({"datetime": datetime.datetime.utcnow()})
+
+def get_texto(request, texto):
+    return HttpResponse("O texto escolhido foi '{}'".format(texto))
