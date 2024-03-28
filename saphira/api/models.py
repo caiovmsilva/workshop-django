59d2a3f238e1bd6fb71eee3bd642251951bb097a
FranciscoGJR <franciscoogjr@gmail.com>
2024-03-28 04:36:33 -0300

    feature: implementando etapa3

--- saphira/api/models.py
+++ saphira/api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    nomeCompleto = models.CharField(max_length=200)
+    email = models.EmailField()
+    cpf = models.CharField(max_length=14)
+
+class Palestra(models.Model):
+    titulo = models.CharField(max_length=200)
+    descricao = models.TextField()
+
+class Presenca(models.Model):
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    palestra = models.ForeignKey(Palestra, on_delete=models.CASCADE)
+    presencial = models.BooleanField(default=False)
