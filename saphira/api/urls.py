d86004af88740b42eb791e0a723d0aaf132a9472
FranciscoGJR <80439645+FranciscoGJR@users.noreply.github.com>
2024-04-20 05:03:18 +0000

    feature: implementando etapa5

--- saphira/api/urls.py
+++ saphira/api/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.get_datetime, name="get_datetime"),
-    path("texto/<texto>", views.get_texto, name="get_texto")
+    path("texto/<texto>", views.get_texto, name="get_texto"),
+    path("usuarios", views.get_usuarios, name="get_usuarios")
 ]
--- saphira/api/views.py
+++ saphira/api/views.py
@@ -1,5 +1,7 @@
 import datetime
 
+from .models import *
+
 from django.http import HttpResponse, JsonResponse
 
 def get_datetime(request):
@@ -7,3 +9,9 @@ def get_datetime(request):
 
 def get_texto(request, texto):
     return HttpResponse("O texto escolhido foi '{}'".format(texto))
+
+def get_usuarios(request):
+
+    usuarios = [usurio.nomeCompleto for usurio in Usuario.objects.all()]
+
+    return JsonResponse(usuarios, safe=False)
