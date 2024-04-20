08f64231d111a5f0fbbde425b8e6dc287c90b657
FranciscoGJR <80439645+FranciscoGJR@users.noreply.github.com>
2024-04-20 07:00:48 +0000

    feature: implementando etapa6

--- saphira/api/urls.py
+++ saphira/api/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.get_datetime, name="get_datetime"),
     path("texto/<texto>", views.get_texto, name="get_texto"),
-    path("usuarios", views.get_usuarios, name="get_usuarios")
+    path("usuarios", views.get_usuarios, name="get_usuarios"),
+    path("usuario/add", views.add_usuario, name="add_usuario")
 ]
--- saphira/api/views.py
+++ saphira/api/views.py
@@ -15,3 +15,15 @@ def get_usuarios(request):
     usuarios = [usurio.nomeCompleto for usurio in Usuario.objects.all()]
 
     return JsonResponse(usuarios, safe=False)
+
+def add_usuario(request):
+
+    nomeCompleto = request.GET['nomeCompleto']
+    email = request.GET['email']
+    cpf = request.GET['cpf']
+
+    novo_usuario = Usuario(nomeCompleto=nomeCompleto, email=email, cpf=cpf)
+
+    novo_usuario.save()
+
+    return JsonResponse({'mensagem':f'{nomeCompleto} cadastrado'})
