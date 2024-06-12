770dcee8ee98e9b24793d0542d182fc3fb8947ba
Caio Vinícius <caiovmsilva123@gmail.com>
2024-06-12 15:28:57 -0300

    update CRUD

--- saphira/api/urls.py
+++ saphira/api/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path("", views.get_datetime, name="get_datetime"),
+    path("texto/<texto>", views.get_texto, name="get_texto"),
+    path("usuarios", views.get_usuarios, name="get_usuarios"),
+    path("usuario/add", views.add_usuario, name="add_usuario"),
+    path("usuario/delete/<cpf>", views.delete_usuario, name="delete_usuario"),
+    path("usuario/update/<cpf>", views.update_usuario, name="update_usuario")
 ]
--- saphira/api/views.py
+++ saphira/api/views.py
@@ -1,7 +1,46 @@
-import datetime
+from datetime import datetime, timezone
 
 from django.http import HttpResponse, JsonResponse
 
-def get_datetime(request):
-    return JsonResponse({"datetime": datetime.now(datetime.UTC)})
+from .models import *
 
+def get_datetime(request):
+    return JsonResponse({"datetime": datetime.now(timezone.utc)})
+
+def get_texto(request, texto):
+    return HttpResponse("O texto escolhido foi '{}'".format(texto))
+
+def get_usuarios(request):
+    usuarios = Usuario.objects.all().values('nomeCompleto', 'cpf', 'email')
+
+    resposta = "= BANCO DE DADOS =\n"
+    for usuario in usuarios:
+        resposta += f"Nome: {usuario['nomeCompleto']}, CPF: {usuario['cpf']}, E-mail: {usuario['email']}\n"
+
+    return HttpResponse(resposta, content_type="text/plain")
+
+def add_usuario(request):
+    nomeCompleto = request.GET['nomeCompleto']
+    email = request.GET['email']
+    cpf = request.GET['cpf']
+
+    novo_usuario = Usuario(nomeCompleto=nomeCompleto, email=email, cpf=cpf)
+
+    novo_usuario.save()
+
+    return JsonResponse({'mensagem': f'{nomeCompleto} cadastrado'})
+
+def delete_usuario(request, cpf):
+    
+    usuario = Usuario.objects.get(cpf=cpf)
+    nomeCompleto = usuario.nomeCompleto
+    usuario.delete()
+
+    return HttpResponse("Usuario {} deletado com sucesso".format(nomeCompleto))
+
+def update_usuario(request, cpf):
+    novo_email = request.GET['email']
+
+    Usuario.objects.filter(cpf=cpf).update(email=novo_email)
+
+    return HttpResponse("Dados do usuario atualizados")
